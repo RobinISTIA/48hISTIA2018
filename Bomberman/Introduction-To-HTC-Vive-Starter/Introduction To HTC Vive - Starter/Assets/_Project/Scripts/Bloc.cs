4b9db253dd47399b4e68fd31f4d8dc4d51eefb83
Unknown <robin.flavigny@free.fr>
2018-02-19 16:27:20 +0100

    Mise en forme du projet + scene

---
+++ Bomberman/Introduction-To-HTC-Vive-Starter/Introduction To HTC Vive - Starter/Assets/_Project/Scripts/Bloc.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bloc : MonoBehaviour {
+
+    public static int nombreDeBloc = 0;
+    public GameObject bloc;
+
+	// Use this for initialization
+	void Start () {
+	    	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
