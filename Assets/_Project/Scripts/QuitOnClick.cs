c14d4d527b46335326237b91845044661618b21b
unknown <usrlocal@GA211-FE18.ad.univ-angers.fr>
2018-02-20 08:43:22 +0100

    Added unfinished MainMenu Scene + unity menu sample

---
+++ Assets/_Project/Scripts/QuitOnClick.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class QuitOnClick : MonoBehaviour
+{
+    private void Start()
+    {
+        gameObject.GetComponent<Button>().onClick.AddListener(Quit);
+    }
+
+    private void Quit()
+    {
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+                Application.Quit ();
+        #endif
+    }
+
+}
