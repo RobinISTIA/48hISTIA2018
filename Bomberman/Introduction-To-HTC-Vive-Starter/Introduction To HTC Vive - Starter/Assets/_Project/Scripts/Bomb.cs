487dbacae64847518a4bf86f95e434dfd7f01261
unknown <usrlocal@GA211-FE01.ad.univ-angers.fr>
2018-02-20 14:27:58 +0100

    Ajout de la class bombe pour le jeu

---
+++ Bomberman/Introduction-To-HTC-Vive-Starter/Introduction To HTC Vive - Starter/Assets/_Project/Scripts/Bomb.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour {
+
+    public int PosX { get; set; }
+    public int PosY { get; set; }
+    public int Range { get; set; }
+    public Plateau plateau;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Detonate()
+    {
+        plateau.Explosion();
+    }
+}
